aba2e010f2ca5bb35a5384c13c6ec9386439aeb0
jtrinity <jtrinity@ufl.edu>
2016-04-26 17:06:24 -0400

    Added Kid Controller

---
+++ Assets/Kid/KidController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KidController : MonoBehaviour {
+
+	public float wait;
+	public float walk;
+	private Animator myAnimator;
+
+
+
+	// Use this for initialization
+	void Start () {
+		myAnimator = GetComponent<Animator> ();
+		myAnimator.SetFloat ("walkspeed", 1.0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
