a07f6518155fac158ddbd423a3fe716b80cdb354
jtrinity <jtrinity@ufl.edu>
2016-04-26 18:39:18 -0400

    Added Teen Models
    Added both teen models as well as idle and walking animations. Athletic
    teen has an additional 'jogging' animation.

---
+++ Assets/Characters/Obese Teen/ObeseTeenController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObeseTeenController : MonoBehaviour {
+
+	private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+	
+		myAnimator = GetComponent<Animator> ();
+		myAnimator.SetFloat ("walkspeed", 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
