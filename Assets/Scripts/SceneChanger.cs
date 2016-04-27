03aaaa4a9c90dbb8641b41be1a25e35314698316
Virada Long <viradalong16@ufl.edu>
2016-04-27 16:10:43 -0400

    title scene

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    //private bool title = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if any key is pressed
+        if(Input.anyKeyDown)
+        {
+            //switch scene to main scene
+            SceneManager.LoadScene("RoomSceneMod");
+        }
+	
+	}
+}
